adf82748456d10936b6b9ec6f8d178238bd554c5
dallinmoak <dallinmoak@gmail.com>
2024-04-11 21:45:31 -0600

    formtating, clarify loader imports

--- webpack/common.config.js
+++ webpack/common.config.js
@@ -1,28 +1,29 @@
-const path = require('path');
+const path = require("path");
 
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: { index: path.resolve(__dirname,'../src','index.js') },
-  mode: 'development',
+  entry: { index: path.resolve(__dirname, "../src", "index.js") },
+  mode: "development",
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: [ 'babel-loader' ]
+        use: {
+          loader: "babel-loader",
+        },
       },
-    ]
+    ],
   },
   optimization: {
-    splitChunks: {chunks: 'all', },
+    splitChunks: { chunks: "all" },
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'React Webpack template',
-      template: path.resolve(__dirname, '../static', 'index.html'),
-      favicon: path.resolve(__dirname, '../static', 'favicon.ico')
+      title: "React Webpack template",
+      template: path.resolve(__dirname, "../static", "index.html"),
+      favicon: path.resolve(__dirname, "../static", "favicon.ico"),
     }),
-  ]
-}
+  ],
+};
